7613c48cfebd9931978f09c23024f3d08d6757a3
Burak <burak.quler@gmail.com>
2023-09-25 21:19:13 +0300

    urls düzenlendi

--- otel/urls.py
+++ otel/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_type>-room/', views.room_overview, name='room-overview'),
     path('<str:room_type>-room/reservation-<int:room_id>/', views.room_reservation, name='room-reservation'),
+    path('<str:room_type>-room/reservation-<int:room_id>/reservation-2/', views.reservation_step_2, name='reservation-step-2'),
+    path('<str:room_type>-room/reservation-<int:room_id>/reservation-2/reservation-3/', views.reservation_step_3, name='reservation-step-3'), 
 ]
