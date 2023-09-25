b71dc3376888761fa6add42732e82b434effe36c
Burak <burak.quler@gmail.com>
2023-09-25 16:24:13 +0300

    urls ve views düzenlendi

--- otel/views.py
+++ otel/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'pages/index-2.html')
+
+def room_overview(request, room_type):
+    room_images = {
+        'single': [
+            "/static/src/images/single1.jpg",
+            "/static/src/images/single2.jpg",
+            "/static/src/images/single3.jpg",
+        ],
+        'classic': [
+            "/static/src/images/classic1.jpg",
+            "/static/src/images/classic2.jpg",
+            "/static/src/images/classic3.jpg",
+        ],
+        'superior': [
+            "/static/src/images/superior3.jpg",
+            "/static/src/images/superior2.jpg",
+            "/static/src/images/superior1.jpg",
+        ],
+        'family-suite': [
+            "/static/src/images/family1.jpg",
+            "/static/src/images/family2.jpg",
+            "/static/src/images/superior-room3.jpg",
+        ],
+    }
+
+    if room_type in room_images:
+        room_image_urls = room_images[room_type]
+    else:
+        room_image_urls = ["/static/src/images/default-room.jpg"]
+
+    context = {
+        'room_image_urls': room_image_urls,
+        'room_type': room_type,
+    }
+    return render(request, 'pages/room-overview.html', context)
