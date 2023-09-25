f6d321300dee083f058dba60faa423a7d4fbf6d6
Burak <burak.quler@gmail.com>
2023-09-25 21:19:34 +0300

    views düzenlendi

--- otel/views.py
+++ otel/views.py
@@ -2,8 +2,43 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 
 
+room_info = {
+    'single': {
+        'name': 'Tek Oda',
+        'image_url': '/static/src/images/single1.jpg',
+    },
+    'classic': {
+        'name': 'Klasik Oda',
+        'image_url': '/static/src/images/classic1.jpg',
+    },
+    'superior': {
+        'name': 'Superior Oda',
+        'image_url': '/static/src/images/superior3.jpg',
+    },
+    'family-suite': {
+        'name': 'Family Suite',
+        'image_url': '/static/src/images/family1.jpg',
+    },
+    'unknown': {
+        'name': 'Bilinmeyen Oda',
+        'image_url': '/static/src/images/default-room.jpg',
+    },
+}
+
 def index(request):
     return render(request, 'pages/index-2.html')
+
+def get_room_info(room_type):
+    return room_info.get(room_type, room_info['unknown'])
+
+def get_common_context(room_type, room_id):
+    room_data = get_room_info(room_type)
+    return {
+        'room_name': room_data['name'],
+        'room_image_url': room_data['image_url'],
+        'room_type': room_type,
+        'room_id': room_id,
+    }
 
 def room_overview(request, room_type):
     room_images = {
@@ -25,7 +60,7 @@ def room_overview(request, room_type):
         'family-suite': [
             "/static/src/images/family1.jpg",
             "/static/src/images/family2.jpg",
-            "/static/src/images/superior-room3.jpg",
+            "/static/src/images/family3.jpg",
         ],
     }
 
@@ -40,28 +75,14 @@ def room_overview(request, room_type):
     }
     return render(request, 'pages/room-overview.html', context)
 
-
-
-
 def room_reservation(request, room_type, room_id):
-    if room_type == 'single':
-        room_name = 'Tek Oda'
-        room_image_url = '/static/src/images/single1.jpg'
-    elif room_type == 'classic':
-        room_name = 'Klasik Oda'
-        room_image_url = '/static/src/images/classic1.jpg' 
-    elif room_type == 'superior':
-        room_name = 'Superior Oda'
-        room_image_url = '/static/src/images/superior3.jpg'
-    elif room_type == 'family-suite':
-        room_name = 'Family Suite'
-        room_image_url = '/static/src/images/family1.jpg'
-    else:
-        room_name = 'Bilinmeyen Oda'
-        room_image_url = '/static/src/images/default-room.jpg'
-
-    context = {
-        'room_name': room_name,
-        'room_image_url': room_image_url,
-    }
+    context = get_common_context(room_type, room_id)
     return render(request, 'pages/reservation-1.html', context)
+
+def reservation_step_2(request, room_type, room_id):
+    context = get_common_context(room_type, room_id)
+    return render(request, 'pages/reservation-2.html', context)
+
+def reservation_step_3(request, room_type, room_id):
+    context = get_common_context(room_type, room_id)
+    return render(request, 'pages/reservation-3.html', context)
