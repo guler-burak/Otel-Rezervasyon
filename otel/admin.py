21e6653db8a1f560ff9e65e8e478403735950550
Burak <burak.quler@gmail.com>
2023-10-28 20:33:49 +0300

    admin.py düzenlendi

--- otel/admin.py
+++ otel/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Room
 
-# Register your models here.
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['room_number','room_type','price']
+
+    class Meta:
+        model = Room
+
+admin.site.register(Room,RoomAdmin)
