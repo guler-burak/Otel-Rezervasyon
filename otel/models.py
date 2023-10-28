8f92b117a6fe586e8bf18b2df3b86b5d25b7b4a7
Burak <burak.quler@gmail.com>
2023-10-28 20:41:49 +0300

    oda modeli oluşturuldu

--- otel/models.py
+++ otel/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+class Room(models.Model):
+    ROOM_TYPES = (
+    ('tek-kisilik', 'Tek Kişilik'),
+    ('çift-aile', 'Çift Kişilik'),
+    ('suit', 'Süit'),
+    ('standart', 'Standart'),
+    ('deluxe', 'Deluxe'),
+)
+
+    room_type = models.CharField(max_length=100, choices=ROOM_TYPES)
+    price = models.PositiveIntegerField()
+    room_number = models.CharField(max_length=10, unique=True)
+    image_home = models.ImageField(upload_to='room_images/home/')
+    image_detail_1 = models.ImageField(upload_to='room_images/detail/')
+    image_detail_2 = models.ImageField(upload_to='room_images/detail/')
+    image_detail_3 = models.ImageField(upload_to='room_images/detail/')
+    description = models.TextField()
+    slug = AutoSlugField(populate_from='room_type', unique=True)
