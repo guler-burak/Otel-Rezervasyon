e5878aa9662a68680930cc01a453476aa0cdbea4
Burak <burak.quler@gmail.com>
2023-11-27 20:02:39 +0300

    sayfalar düzenlendi

--- otel/views.py
+++ otel/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.conf import settings
 from .models import Room, Contact
 from .forms import ContactForm, RegisterForm, LoginForm
 from django.contrib import messages
+from .forms import ReservationForm, Customer
+from datetime import datetime
 
 def index(request):
     rooms = Room.objects.all()
@@ -16,15 +18,21 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Hesap başarıyla oluşturuldu. Şimdi giriş yapabilirsiniz.')
+            user = form.save(commit=False)
+            user.first_name = form.cleaned_data['ad']
+            user.last_name = form.cleaned_data['soyad']
+            user.save()
+
+            Customer.objects.create(user=user, ad=form.cleaned_data['ad'],
+                                    soyad=form.cleaned_data['soyad'],
+                                    telefon=form.cleaned_data['telefon'])
             return redirect('index')
         else:
             messages.error(request, 'Lütfen formu doğru bir şekilde doldurun.') 
     else:
         form = RegisterForm()
 
-    return render(request, 'pages/register.html', {'form': form}) 
+    return render(request, 'pages/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
@@ -32,13 +40,16 @@ def login_view(request):
         if form.is_valid():
             if user := authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password']):
                 login(request, user)
-                messages.success(request, 'Başarıyla giriş yaptınız.')
                 return redirect('index')
             messages.error(request, 'Geçersiz kullanıcı adı veya şifre.')
     else:
         form = LoginForm()
 
     return render(request, 'pages/login.html', {'login_form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
 
 def category(request):
     return render(request, 'pages/rooms-category.html')
@@ -51,7 +62,6 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Her zaman yeni bir kayıt oluştur
             Contact.objects.create(
                 name=form.cleaned_data['name'],
                 surname=form.cleaned_data['surname'],
@@ -60,53 +70,75 @@ def contact(request):
                 message=form.cleaned_data['message']
             )
             messages.success(request, 'Mesajınız başarıyla gönderildi.')
-            return redirect('index')  # Başarı sayfasına yönlendirme
+            return redirect('index')
     else:
         form = ContactForm()
 
     return render(request, 'pages/contact.html', {'form': form})
 
-def get_room_info(room_type):
-    return settings.ROOM_INFO.get(room_type, settings.ROOM_INFO['unknown'])
-
-def get_common_context(room_type, room_id):
-    room_data = get_room_info(room_type)
-    return {
-        'room_name': room_data['name'],
-        'room_image_url': room_data['image_url'],
-        'room_type': room_type,
-        'room_id': room_id,
-    }
 
 def room_overview(request, room_slug):
     room = get_object_or_404(Room, slug=room_slug)
     context = {'room': room}
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            checkin_date = form.cleaned_data['checkin_date']
+            checkout_date = form.cleaned_data['checkout_date']
+
+            context = {
+                'checkin_date': checkin_date,
+                'checkout_date': checkout_date,
+            }
+            return render(request, 'pages/reservation-1.html', context)
+
+    else:
+        form = ReservationForm()
+
+    context['form'] = form
     return render(request, 'pages/room-overview.html', context)
+
 
 def reservation(request, room_slug):
     room = get_object_or_404(Room, slug=room_slug)
-    context = {'room': room}
-    return render(request, 'pages/reservation-1.html', context)
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            checkin_date = form.cleaned_data['checkin_date']
+            checkout_date = form.cleaned_data['checkout_date']
+
+            request.session['checkin_date'] = checkin_date.strftime("%Y-%m-%d")
+            request.session['checkout_date'] = checkout_date.strftime("%Y-%m-%d")
+
+            return render(request, 'pages/reservation-1.html', {'room': room, 'checkin_date': checkin_date, 'checkout_date': checkout_date})
+
+    else:
+        form = ReservationForm()
+
+    return render(request, 'pages/reservation-1.html', {'room': room, 'form': form})
 
 def reservation_step_2(request, room_slug):
     room = get_object_or_404(Room, slug=room_slug)
 
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Hesap başarıyla oluşturuldu. Şimdi giriş yapabilirsiniz.')
-            return redirect('login')
-        else:
-            messages.error(request, 'Lütfen formu doğru bir şekilde doldurun.')
-    else:
-        form = RegisterForm()
 
-    context = {'room': room, 'form': form}
-    return render(request, 'pages/reservation-2.html', context)
+    checkin_date_str = request.session.get('checkin_date')
+    checkout_date_str = request.session.get('checkout_date')
+
+
+    checkin_date = datetime.strptime(checkin_date_str, "%Y-%m-%d") if checkin_date_str else None
+    checkout_date = datetime.strptime(checkout_date_str, "%Y-%m-%d") if checkout_date_str else None
+
+    return render(request, 'pages/reservation-2.html', {'room': room, 'checkin_date': checkin_date, 'checkout_date': checkout_date})
 
 def reservation_step_3(request, room_slug):
     room = get_object_or_404(Room, slug=room_slug)
-    context = {'room': room}
-    return render(request, 'pages/reservation-3.html', context)
 
+    checkin_date_str = request.session.get('checkin_date')
+    checkout_date_str = request.session.get('checkout_date')
+
+    checkin_date = datetime.strptime(checkin_date_str, "%Y-%m-%d") if checkin_date_str else None
+    checkout_date = datetime.strptime(checkout_date_str, "%Y-%m-%d") if checkout_date_str else None
+
+    return render(request, 'pages/reservation-3.html', {'room': room, 'checkin_date': checkin_date, 'checkout_date': checkout_date})
