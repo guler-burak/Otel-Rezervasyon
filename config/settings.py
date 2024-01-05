86922f5324f7bb8aa07a0c792053aa482544f5df
Burak Güler <burak.quler@gmail.com>
2024-01-05 15:07:59 +0300

    captcha eklendi

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'otel',
+    'captcha'
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('otel.urls')),
     
 ]
